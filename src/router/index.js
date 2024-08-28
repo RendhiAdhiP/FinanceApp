e65cbe7781a5406f204655523b4546943fe80d67
RendhiAdhiP <rendhiap123@gmail.com>
2024-08-28 16:19:43 +0700

    update: update routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,62 +1,36 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-import EmailAuth from '@/views/auth/Login/Email/Index.vue'
-import ResetPssword from '@/views/auth/Login/Email/ResetPssword.vue'
-import SuccessResetPassword from '@/views/auth/Login/Email/SuccessResetPassword.vue'
-import Verified from '@/views/auth/Login/Email/Verified.vue'
-import Dashboard from '@/views/pages/Dashboard/Index.vue'
-import Amount from '@/views/pages/Dashboard/SendingMoney/Amount.vue'
-import PersonalDetails from '@/views/pages/Dashboard/SendingMoney/PersonalDetails.vue'
-import Recipient from '@/views/pages/Dashboard/SendingMoney/Recipient.vue'
-import ReviewAndPay from '@/views/pages/Dashboard/SendingMoney/ReviewAndPay.vue'
-import SuccessSendingMoney from '@/views/pages/Dashboard/SendingMoney/Success.vue'
-import Balance from '@/views/pages/Balance/Index.vue'
-import Transaction from '@/views/pages/Transaction/Index.vue'
-import Cards from '@/views/pages/Cards/Index.vue'
-import Onboarding from '@/views/pages/Cards/GettingPhysicalCards/Onboarding.vue'
-import Address from '@/views/pages/Cards/GettingPhysicalCards/Address.vue'
-import VerificationCard from '@/views/pages/Cards/GettingPhysicalCards/VerificationCard.vue'
-import Payment from '@/views/pages/Cards/GettingPhysicalCards/Payment.vue'
-import SuccessAddCard from '@/views/pages/Cards/GettingPhysicalCards/SuccessAddCard.vue'
-import Recepients from '@/views/pages/Recepients/Index.vue'
-import Integrations from '@/views/pages/Integrations/Index.vue'
-import Settings from '@/views/pages/Settings/Index.vue'
-
-const Register = () => import('../views/auth/Register/Index.vue')
-const Login = () => import('@/views/auth/Login/Index.vue')
-const AccountSetting = () => import('@/views/auth/Register/Onboarding/Index.vue')
-
 const routes = [
-    { path: '/', component: Login },
+    { path: '/', component: () => import('@/views/auth/Login/Index.vue') },
+    
+    { path: '/register', component: () => import('../views/auth/Register/Index.vue') },
+    { path: '/register/onboarding', component: () => import('@/views/auth/Register/Onboarding/Index.vue') },
+    { path: '/register/email', component: () => import('@/views/auth/Login/Email/Index.vue') },
+    { path: '/register/email/verify', component: () => import('@/views/auth/Login/Email/Verified.vue') },
+    { path: '/reset-password', component: () => import('@/views/auth/Login/Email/ResetPssword.vue') },
+    { path: '/reset-password/success', component: () => import('@/views/auth/Login/Email/SuccessResetPassword.vue') },
 
-    { path: '/register', component: Register },
-    { path: '/register/onboarding', component: AccountSetting },
-    { path: '/register/email', component: EmailAuth },
-    { path: '/register/email/verify', component: Verified },
-    { path: '/reset-password', component: ResetPssword },
-    { path: '/reset-password/success', component: SuccessResetPassword },
+    { path: '/dashboard', component: () => import('@/views/pages/Dashboard/Index.vue') },
 
-    { path: '/dashboard', component: Dashboard },
+    { path: '/sending-money/amount', component: () => import('@/views/pages/Dashboard/SendingMoney/Amount.vue') },
+    { path: '/sending-money/personal-details', component: () => import('@/views/pages/Dashboard/SendingMoney/PersonalDetails.vue') },
+    { path: '/sending-money/recipient', component: () => import('@/views/pages/Dashboard/SendingMoney/Recipient.vue') },
+    { path: '/sending-money/review-and-pay', component: () => import('@/views/pages/Dashboard/SendingMoney/ReviewAndPay.vue') },
+    { path: '/sending-money/success', component: () => import('@/views/pages/Dashboard/SendingMoney/Success.vue') },
 
-    { path: '/sending-money/amount', component: Amount },
-    { path: '/sending-money/personal-details', component: PersonalDetails },
-    { path: '/sending-money/recipient', component: Recipient },
-    { path: '/sending-money/review-and-pay', component: ReviewAndPay },
-    { path: '/sending-money/success', component: SuccessSendingMoney },
+    { path: '/balance', component: () => import('@/views/pages/Balance/Index.vue') },
+    { path: '/transactions', component: () => import('@/views/pages/Transaction/Index.vue') },
 
-    { path: '/balance', component: Balance },
-    { path: '/transactions', component: Transaction },
+    { path: '/cards', component: () => import('@/views/pages/Cards/Index.vue') },
+    { path: '/cards/add-card/onboarding', component: () => import('@/views/pages/Cards/GettingPhysicalCards/Onboarding.vue') },
+    { path: '/cards/add-card/address', component: () => import('@/views/pages/Cards/GettingPhysicalCards/Address.vue') },
+    { path: '/cards/add-card/verification', component: () => import('@/views/pages/Cards/GettingPhysicalCards/VerificationCard.vue') },
+    { path: '/cards/add-card/payment', component: () => import('@/views/pages/Cards/GettingPhysicalCards/Payment.vue') },
+    { path: '/cards/add-card/success', component: () => import('@/views/pages/Cards/GettingPhysicalCards/SuccessAddCard.vue') },
 
-    { path: '/cards', component: Cards },
-    { path: '/cards/add-card/onboarding', component: Onboarding },
-    { path: '/cards/add-card/address', component: Address },
-    { path: '/cards/add-card/verification', component: VerificationCard },
-    { path: '/cards/add-card/payment', component: Payment },
-    { path: '/cards/add-card/success', component: SuccessAddCard },
-
-    { path: '/recepients', component: Recepients },
-    { path: '/integrations', component: Integrations },
-    { path: '/settings', component: Settings },
+    { path: '/recepients', component: () => import('@/views/pages/Recepients/Index.vue') },
+    { path: '/integrations', component: () => import('@/views/pages/Integrations/Index.vue') },
+    { path: '/settings', component: () => import('@/views/pages/Settings/Index.vue') },
 ]
 
 const router = createRouter({
